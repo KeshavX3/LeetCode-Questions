76277a5612cb14a6780c6432f9cdb6dcfb86ba7e
keshavx3 <keshavvr1@gmail.com>
2024-10-07 21:07:33 +0530

    94

---
+++ Q94.java
@@ -0,0 +1,45 @@
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
+
+// Example 2:
+// Input: root = []
+// Output: []
+
+// Example 3:
+// Input: root = [1]
+// Output: [1]
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Define TreeNode class within the same file for simplicity
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Q94 {
+    public List<Integer> list = new ArrayList<Integer>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+            return list;
+        } else {
+            return new ArrayList<Integer>();
+        }
+    }
+}
